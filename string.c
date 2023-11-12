0f6c5b7dfbd222dfdbd86526a7e0fea20d9e7d63
UDO MARY <111148417+ImeGits@users.noreply.github.com>
2023-11-12 12:01:14 +0100

    Update string.c

--- string.c
+++ string.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * @s: the string whose length to check.(group project)
  *
  * Return: integer length of string
  */
@@ -21,7 +21,7 @@ int _strlen(char *s)
 /**
  * _strcmp - performs lexicogarphic comparison of two strangs.
  * @s1: the first strang
- * @s2: the second strang
+ * @s2: the second strang.(group project)
  *
  * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
@@ -43,7 +43,7 @@ int _strcmp(char *s1, char *s2)
 /**
  * starts_with - checks if needle starts with haystack
  * @haystack: string to search
- * @needle: the substring to find
+ * @needle: the substring to find.(group project)
  *
  * Return: address of next char of haystack or NULL
  */
@@ -58,7 +58,7 @@ char *starts_with(const char *haystack, const char *needle)
 /**
  * _strcat - concatenates two strings
  * @dest: the destination buffer
- * @src: the source buffer
+ * @src: the source buffer.(group project)
  *
  * Return: pointer to destination buffer
  */
