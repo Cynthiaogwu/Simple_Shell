4af931fcb5134919c71246f279d76de4f8eea3cd
UDO MARY <111148417+ImeGits@users.noreply.github.com>
2023-11-12 11:55:04 +0100

    Update builtin1.c

--- builtin1.c
+++ builtin1.c
@@ -3,7 +3,7 @@
 /**
  * _myexit - exits the shell
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *          constant function prototype. (group project)
  *  Return: exits with a given exit status
  *         (0) if info.argv[0] != "exit"
  */
@@ -32,7 +32,7 @@ int _myexit(info_t *info)
 /**
  * _mycd - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *          constant function prototype. (group project)
  *  Return: Always 0
  */
 int _mycd(info_t *info)
@@ -82,7 +82,7 @@ int _mycd(info_t *info)
 /**
  * _myhelp - changes the current directory of the process
  * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ *          constant function prototype. (group project)
  *  Return: Always 0
  */
 int _myhelp(info_t *info)
