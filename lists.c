abab95b680e7592468e536ce27ba247e50f5b2cc
UDO MARY <111148417+ImeGits@users.noreply.github.com>
2023-11-12 12:14:38 +0100

    Update lists.c

--- lists.c
+++ lists.c
@@ -4,7 +4,7 @@
  * add_node - adds a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
- * @num: node index used by history
+ * @num: node index used by history.(group project)
  *
  * Return: size of list
  */
@@ -37,7 +37,7 @@ list_t *add_node(list_t **head, const char *str, int num)
  * add_node_end - adds a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
- * @num: node index used by history
+ * @num: node index used by history.(group project)
  *
  * Return: size of list
  */
@@ -76,7 +76,7 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 
 /**
  * print_list_str - prints only the str element of a list_t linked list
- * @h: pointer to first node
+ * @h: pointer to first node.(group project)
  *
  * Return: size of list
  */
@@ -97,7 +97,7 @@ size_t print_list_str(const list_t *h)
 /**
  * delete_node_at_index - deletes node at given index
  * @head: address of pointer to first node
- * @index: index of node to delete
+ * @index: index of node to delete.(group project)
  *
  * Return: 1 on success, 0 on failure
  */
@@ -136,7 +136,7 @@ int delete_node_at_index(list_t **head, unsigned int index)
 
 /**
  * free_list - frees all nodes of a list
- * @head_ptr: address of pointer to head node
+ * @head_ptr: address of pointer to head node.(group project)
  *
  * Return: void
  */
