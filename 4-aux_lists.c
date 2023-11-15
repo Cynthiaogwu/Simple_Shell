9973be24d6b651bec2f276a2569bbc7ed5c38796
Ogwu Cynthia Isioma <141334276+Cynthiaogwu@users.noreply.github.com>
2023-11-16 00:37:04 +0100

    Update 4-aux_lists.c

--- 4-aux_lists.c
+++ 4-aux_lists.c
@@ -5,7 +5,7 @@
  * of a sep_list.
  * @head: head of the linked list.
  * @sep: separator found (; | &).
- * Return: address of the head.
+ * Return: head of address.
  */
 sep_list *add_sep_node_end(sep_list **head, char sep)
 {
@@ -87,7 +87,6 @@ line_list *add_line_node_end(line_list **head, char *line)
 
 	return (*head);
 }
-
 /**
  * free_line_list - frees a line_list
  * @head: head of the linked list.
