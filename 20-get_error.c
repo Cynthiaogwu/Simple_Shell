a0ec7bbfb2f871aeacae456a1585b38e35cbfb67
Ogwu Cynthia Isioma <141334276+Cynthiaogwu@users.noreply.github.com>
2023-11-16 00:59:24 +0100

    simple

--- 20-get_error.c
+++ 20-get_error.c
@@ -1,9 +1,8 @@
 #include "shell.h"
-
 /**
- * get_error - calls the error according the builtin, syntax or permission
- * @datash: data structure that contains arguments
- * @eval: error value
+ * get_error - calls the error according the builtin, syntax or permission.
+ * @datash: data structure that contains arguments.
+ * @eval: error value.
  * Return: error
  */
 int get_error(data_shell *datash, int eval)
