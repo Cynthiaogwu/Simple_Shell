cf1032b71c7baefd718c0a30c5c24256cb655c79
Ogwu Cynthia Isioma <141334276+Cynthiaogwu@users.noreply.github.com>
2023-11-16 00:34:28 +0100

    Update 5-aux_lists.c

--- 5-aux_lists.c
+++ 5-aux_lists.c
@@ -1,5 +1,4 @@
 #include "shell.h"
-
 /**
  * add_rvar_node - adds a variable at the end
  * of a r_var list.
@@ -39,7 +38,7 @@ r_var *add_rvar_node(r_var **head, int lvar, char *val, int lval)
 }
 
 /**
- * free_rvar_list - frees a r_var list
+ * free_rvar_list - frees a r_var list.
  * @head: head of the linked list.
  * Return: no return.
  */
