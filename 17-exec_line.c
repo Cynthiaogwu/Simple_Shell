a95a8dda12d570d3b2a57b24ec1aea0ba06232a0
Ogwu Cynthia Isioma <141334276+Cynthiaogwu@users.noreply.github.com>
2023-11-16 01:00:00 +0100

    simple

--- 17-exec_line.c
+++ 17-exec_line.c
@@ -1,9 +1,8 @@
 #include "shell.h"
-
 /**
- * exec_line - finds builtins and commands
+ * exec_line - finds builtins and commands.
  *
- * @datash: data relevant (args)
+ * @datash: data relevant (args).
  * Return: 1 on success.
  */
 int exec_line(data_shell *datash)
