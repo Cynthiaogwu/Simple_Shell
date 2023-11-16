0dd53d520c31990ea95a0c4dd13e8e664e3cf81e
Ogwu Cynthia Isioma <141334276+Cynthiaogwu@users.noreply.github.com>
2023-11-16 01:02:49 +0100

    simple

--- 18-exit_shell.c
+++ 18-exit_shell.c
@@ -1,9 +1,8 @@
 #include "shell.h"
-
 /**
- * exit_shell - exits the shell
+ * exit_shell - exits the shell.
  *
- * @datash: data relevant (status and args)
+ * @datash: data relevant (status and args).
  * Return: 0 on success.
  */
 int exit_shell(data_shell *datash)
